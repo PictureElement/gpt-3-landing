36d0250240eae14d54ed94de5253966ec7844217
PictureElement <marios.sofokleous@yandex.com>
2021-12-16 22:34:33 +0200

    Add gradients to background and surface components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import './App.scss';
 import { Header, Hero, SocialProof, About, Footer } from './components';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
+    <div className="app">
+      <div className="app__gradient">
+        <Header />
+        <Hero />
+      </div>
       <SocialProof />
       <About />
       <Footer />
