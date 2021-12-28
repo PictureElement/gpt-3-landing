55e7a6d21a6f183ae7f7ba8752ed4897a0e87296
PictureElement <marios.sofokleous@yandex.com>
2021-12-28 13:59:51 +0200

    Make hero image responsive with srcset and sizes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -25,12 +25,40 @@ function Hero() {
             <button className="subscribe-form__submit" tabIndex="0" type="submit">Get started</button>
           </form>
           <div className="hero__users">
-            <img src={users} alt="..." />
+            <img width="182" height="39" src={users} alt="..." />
             <p>1,600 users have been approved for early access in the past 24h.</p>
           </div>
         </div>
         <div className="hero__right">
-          <img className="scale-up-center" src={ai} alt="..." />
+          <img width="1" height="1" src={ai} alt="..." />
+          {/* <img
+            width="1"
+            height="1"
+            src="https://dummyimage.com/706"
+            srcSet="
+              https://dummyimage.com/706 706w,
+              https://dummyimage.com/1412 1412w,
+              https://dummyimage.com/591 591w,
+              https://dummyimage.com/1182 1182w,
+              https://dummyimage.com/688 688w,
+              https://dummyimage.com/1376 1376w,
+              https://dummyimage.com/508 508w,
+              https://dummyimage.com/1016 1016w,
+              https://dummyimage.com/414 414w,
+              https://dummyimage.com/828 828w,
+              https://dummyimage.com/375 375w,
+              https://dummyimage.com/750 750w,
+              https://dummyimage.com/360 360w,
+              https://dummyimage.com/720 720w
+            "
+            sizes="
+              (min-width: 1200px) 706px,
+              (min-width: 992px) 591px,
+              (min-width: 768px) 688px,
+              (min-width: 576px) 508px,
+              calc(100vw - 32px)"
+            alt="..."
+          /> */}
         </div>
       </div>
     </section>
