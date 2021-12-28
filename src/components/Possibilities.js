df41ee718aaf0dbd5feb7ab8a40f2f612eaaa42b
PictureElement <marios.sofokleous@yandex.com>
2021-12-28 15:04:24 +0200

    Make posibilities image responsive

--- src/components/Possibilities.js
+++ src/components/Possibilities.js
@@ -9,7 +9,37 @@ function Possibilities() {
       <div className="container">
         <div className="possibilities__surface">
           <div className="possibilities__left">
-            <img className="possibilities__image" src={possibilities} alt="..." />
+            <img width="1" height="1" className="possibilities__image" src={possibilities} loading="lazy" alt="..." />
+            {/* <img
+              className="possibilities__image"
+              width="1"
+              height="1"
+              src="https://dummyimage.com/426"
+              srcSet="
+                https://dummyimage.com/426 426w,
+                https://dummyimage.com/852 852w,
+                https://dummyimage.com/353 353w,
+                https://dummyimage.com/706 706w,
+                https://dummyimage.com/251 251w,
+                https://dummyimage.com/502 502w,
+                https://dummyimage.com/508 508w,
+                https://dummyimage.com/1016 1016w,
+                https://dummyimage.com/414 414w,
+                https://dummyimage.com/828 828w,
+                https://dummyimage.com/375 375w,
+                https://dummyimage.com/750 750w,
+                https://dummyimage.com/360 360w,
+                https://dummyimage.com/720 720w
+              "
+              sizes="
+                (min-width: 1200px) 426px,
+                (min-width: 992px) 353px,
+                (min-width: 768px) 251px,
+                (min-width: 576px) 508px,
+                calc(100vw - 32px)"
+              loading="lazy"
+              alt="..."
+            /> */}
           </div>
           <div className="possibilities__right">
             <div className="possibilities__introduction">
