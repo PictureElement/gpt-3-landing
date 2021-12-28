3f4fb7f9d16a12ef9aaeaadc2d993bc19a6f6d4f
PictureElement <marios.sofokleous@yandex.com>
2021-12-28 15:43:49 +0200

    Make post image responsive

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -16,7 +16,39 @@ function Blog() {
             <li className="media-list__item media-list__item_1">
               <a href="." className="media-list__link">
                 <article className="post">
-                  <img src={postImage} className="post__image" alt="..." />
+                  <div className="post__image-wrapper">
+                    <img className="post__image" width="16" height="9" src={postImage} loading="lazy" alt="..." />
+                    {/* <img
+                      className="post__image"
+                      width="16"
+                      height="9"
+                      src="https://dummyimage.com/522x294"
+                      srcSet="
+                        https://dummyimage.com/522x294 522w,
+                        https://dummyimage.com/1044x587 1044w,
+                        https://dummyimage.com/434x244 434w,
+                        https://dummyimage.com/868x488 868w,
+                        https://dummyimage.com/316x178 316w,
+                        https://dummyimage.com/632x356 632w,
+                        https://dummyimage.com/508x286 508w,
+                        https://dummyimage.com/1016x572 1016w,
+                        https://dummyimage.com/414x233 414w,
+                        https://dummyimage.com/828x466 828w,
+                        https://dummyimage.com/375x211 375w,
+                        https://dummyimage.com/750x422 750w,
+                        https://dummyimage.com/360x203 360w,
+                        https://dummyimage.com/720x405 720w
+                      "
+                      sizes="
+                        (min-width: 1200px) 522px,
+                        (min-width: 992px) 434px,
+                        (min-width: 768px) 316px,
+                        (min-width: 576px) 508px,
+                        calc(100vw - 32px)"
+                      loading="lazy"
+                      alt="..."
+                    /> */}
+                  </div>
                   <div className="post__body">
                     <div>
                       <div className="post__date">Jun 24, 2021</div>
